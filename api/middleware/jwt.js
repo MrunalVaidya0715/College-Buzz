a5e340c449cb234921ce121895a4b9241f80df07
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-26 00:44:31 +0530

    fixed mddlwre

--- api/middleware/jwt.js
+++ api/middleware/jwt.js
@@ -7,7 +7,6 @@ export const verifyToken = (req, res, next) => {
   jwt.verify(token, process.env.JWT_KEY, async(err, payload)=>{
     if (err) return next(createError(403,"Token is Invalid"))
     req.userId = payload.id
-    req.isSeller = payload.isSeller
     next()
 })
 };
