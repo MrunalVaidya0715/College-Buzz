cb3e4c5f4ea9fbbd3eeb02e40578333797eda4be
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-25 18:55:49 +0530

    basic server setup

---
+++ api/server.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+import mongoose from 'mongoose';
+const app = express()
+dotenv.config()
+mongoose.set('strictQuery',true)
+
+const connect = async() =>{
+    try{
+        await mongoose.connect(process.env.MONGO)
+        console.log("Connected to MongoDB")
+    
+    }catch(err){
+        console.log(err)
+    }
+}
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(express.json())
+app.use(cookieParser())
+
+
+
+app.use((err,req,res,next)=>{
+    const errStatus = err.status || 500
+    const errMessage = err.message || "Something went Wrong"
+    return res.status(errStatus).send(errMessage)
+})
+
+
+const port = process.env.PORT || 8800
+app.listen(port,()=>{
+    connect()
+    console.log(`Backend is Running at ${port}`)
+})
