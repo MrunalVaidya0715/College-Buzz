8e2933ce48bf4b5307bedbf4d8bd953112a5ba08
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-25 19:40:31 +0530

    basic auth route setup

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,10 @@
-import { Router } from "express";
-import { signIn } from "../controllers/auth.controller.js";
+import { Router } from 'express'
+import { register, login, logout } from '../controllers/auth.controller.js'
+const router  = Router()
 
-const router = Router();
+router.post('/register',register)
+router.post('/login',login)
+router.post('/logout',logout)
 
-router.get("/", signIn)
 
 export default router;
