2ae7b6fd9b1e8968a3bd0f07ef9aad3b0ef2c595
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-26 20:57:00 +0530

    basic getQuestions setup

--- api/controllers/question.controller.js
+++ api/controllers/question.controller.js
@@ -11,8 +11,19 @@ export const createQuestion = async (req, res, next) => {
 
   try {
     const savedQuestion = await newQuestion.save();
-    res.status(201).json(savedQuestion);
+    res.status(201).send(savedQuestion);
   } catch (err) {
     next(err);
+  }
+};
+
+export const getQuestions = async (req, res, next) => {
+  try {
+
+    const questions = await Question.find();
+
+    res.status(200).json(questions);
+  } catch (error) {
+    next(error);
   }
 };
--- api/routes/question.route.js
+++ api/routes/question.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { createQuestion } from "../controllers/question.controller.js";
+import { createQuestion, getQuestions } from "../controllers/question.controller.js";
 import { verifyToken } from "../middleware/jwt.js";
 
 const router = Router();
 
 router.post("/", verifyToken, createQuestion)
-
+router.get("/", getQuestions)
 export default router;
