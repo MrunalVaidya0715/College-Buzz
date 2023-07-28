8cb6160fdfe225f83bad0ea8a1d35c75657f2a1a
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-28 13:52:35 +0530

    Filtering Posts

--- client/src/pages/myQuestions/MyQuestions.jsx
+++ client/src/pages/myQuestions/MyQuestions.jsx
@@ -1,6 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../../utils/newRequest";
+
 const MyQuestions = () => {
+  const user = JSON.parse(localStorage.getItem("currentUser"))
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["userQuestion"],
+    queryFn: () =>
+      newRequest.get(`/questions`).then((res) => {
+        return res.data;
+      }),
+  });
+  const filterPosts = data.filter((post)=> post.userInfo._id === user?._id)
   return (
-    <div>MyQuestions</div>
+    <div className="flex w-full flex-col h-full bg-white">
+      <h1 className=" text-3xl font-semibold text-center my-2">Your Questions</h1>
+      
+    </div>
   )
 }
 
