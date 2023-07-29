f6d6e28557ea75658e1be455aa2c1eb2843b7c09
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-29 19:03:07 +0530

    some UI changes

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -16,7 +16,7 @@ const Reviews = () => {
   return (
     <div className=" mt-8 w-full flex flex-col gap-2">
       {/* Render Reviews */}
-      <div className=" relative h-auto w-full flex flex-col gap-4 md:gap-8">
+      <div className=" relative h-auto w-full flex flex-col gap-2 md:gap-4 ">
         {
           isLoading ? (<h2 className=" text-center">Loading Answers...</h2>) :
             error ? (<h2 className=" text-center">Something went wrong</h2>) :
