05f73bd88ccd8f71cc04a7a8702b5674bdb251b5
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-29 22:03:41 +0530

    updated order/naming

--- api/routes/answer.route.js
+++ api/routes/answer.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createAnswer, deleteAnswer, getAnswersByQuesId, handleDownvote, handleUpvote } from "../controllers/answer.controller.js";
+import { createAnswer, deleteAnswer, getAnswersByQuesId, getAnswersByUserId, handleDownvote, handleUpvote } from "../controllers/answer.controller.js";
 import { verifyToken } from "../middleware/jwt.js";
 
 const router = Router();
 
-router.get("/:quesId", getAnswersByQuesId)
+router.get("/question/:quesId", getAnswersByQuesId)
 router.post("/", verifyToken, createAnswer)
+router.get("/user/:userId", getAnswersByUserId)
 router.patch("/upvote/:id", verifyToken, handleUpvote)
 router.patch("/downvote/:id", verifyToken, handleDownvote)
 router.delete("/:ansId", verifyToken, deleteAnswer)
