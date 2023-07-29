7aa3bd427233013d144afc1d22ed770a173f7a0b
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-30 01:45:27 +0530

    deleteUser

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,7 +3,6 @@ import createError from "../utils/createError.js";
 import mongoose from "mongoose";
 export const getUser = async (req, res, next) => {
   try {
-    
     const userId = req.params.userId;
     if (!mongoose.Types.ObjectId.isValid(userId)) {
       // If the questionId is not a valid ObjectId
@@ -14,6 +13,34 @@ export const getUser = async (req, res, next) => {
       return next(createError(404, "User not Found"));
     }
     res.status(200).send(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return next(createError(404, "User not found"));
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return next(createError(404, "User not found"));
+    }
+
+    if (req.userId !== user._id.toString()) {
+      return next(createError(403, "You can delete only your account!"));
+    }
+
+    await Question.updateMany({ userId }, { $set: { userId: null, userInfo: null } });
+  
+    await Answer.updateMany({ userId }, { $set: { userId: null, userInfo: null } });
+
+    await User.findByIdAndDelete(userId);
+
+    res.status(200).json({ message: "User account deleted successfully" });
   } catch (error) {
     next(error);
   }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getUser } from "../controllers/user.controller.js";
+import { deleteUser, getUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
 
 const router = Router();
 
+router.get("/delete/:userId", verifyToken, deleteUser)
 router.get("/:userId", getUser)
 
 export default router;
