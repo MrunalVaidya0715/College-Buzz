c473e01e2a8e8a553ebf54998fd01f553bf239c3
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-30 20:00:58 +0530

    handled userLog

--- client/src/components/AskButton.jsx
+++ client/src/components/AskButton.jsx
@@ -1,11 +1,30 @@
-import {IoMdAdd} from 'react-icons/io'
-const AskButton = ({ask, handleAsk}) => {
+import { IoMdAdd } from 'react-icons/io'
+import { Link } from 'react-router-dom'
+const AskButton = ({ ask, handleAsk }) => {
+    const user = JSON.parse(localStorage.getItem("currentUser"))
 
-  return (
-    <div onClick={handleAsk} className={` ${ask ? "hidden": "block"} cursor-pointer z-[200] border-[1px] border-gray-400 md:hidden absolute bottom-4 right-2 p-4 bg-blue-500 rounded-full`}>
-        <IoMdAdd className=' text-white' size={22}/>
-    </div>
-  )
+    return (
+        <>
+            {
+                user ? (
+                    <>
+                        <div onClick={handleAsk} className={` ${ask ? "hidden" : "block"} cursor-pointer z-[200] md:hidden absolute bottom-4 right-2 p-4 bg-blue-500 rounded-full`}>
+                            <IoMdAdd className=' text-white' size={22} />
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/login">
+                            <div  className={` ${ask ? "hidden" : "block"} cursor-pointer z-[200] md:hidden absolute bottom-4 right-2 p-4 bg-blue-500 rounded-full`}>
+                                <IoMdAdd className=' text-white' size={22} />
+                            </div>
+                        </Link>
+                    </>
+                )
+            }
+        </>
+
+    )
 }
 
 export default AskButton
