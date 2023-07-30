1b1365e0601dc082b39ead937ea4db566ad8cd94
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-30 17:30:26 +0530

    google Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,28 +13,32 @@ import Profile from './pages/profile/Profile';
 import MyQuestions from './pages/myQuestions/MyQuestions';
 import Explore from './pages/explore/Explore';
 import Contribute from './pages/contribute/Contribute';
-
+import { GoogleOAuthProvider } from '@react-oauth/google'
 function App() {
 
   const queryClient = new QueryClient()
+  const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
   return (
     <Router>
       <div className=" min-h-screen flex w-full flex-col justify-between">
         <QueryClientProvider client={queryClient}>
-          <Navbar />
-          <Routes>
-            <Route exact path='/' element={< Home />}>
-              <Route path='' element={<Posts />} />
-              <Route path='/explore' element={<Explore />} />
-              <Route path='/contribute' element={<Contribute />} />
-              <Route path='/posts/:id' element={<Post />} />
-              <Route path='/profile/:userId' element={<Profile />} />
-              <Route path='/my-questions/:userId' element={<MyQuestions/>} />
-            </Route>
+          <GoogleOAuthProvider clientId={clientId}>
+            <Navbar />
+            <Routes>
+              <Route exact path='/' element={< Home />}>
+                <Route path='' element={<Posts />} />
+                <Route path='/explore' element={<Explore />} />
+                <Route path='/contribute' element={<Contribute />} />
+                <Route path='/posts/:id' element={<Post />} />
+                <Route path='/profile/:userId' element={<Profile />} />
+                <Route path='/my-questions/:userId' element={<MyQuestions />} />
+              </Route>
 
-            <Route exact path='/login' element={< Login />}></Route>
-            <Route exact path='/register' element={< Register />}></Route>
-          </Routes>
+              <Route exact path='/login' element={< Login />}></Route>
+              <Route exact path='/register' element={< Register />}></Route>
+            </Routes>
+          </GoogleOAuthProvider>
         </QueryClientProvider>
       </div>
 
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,69 +1,47 @@
-import { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import newRequest from "../../../utils/newRequest.js"
+import { GoogleLogin } from "@react-oauth/google"
+import jwt_decode from 'jwt-decode'
 const Login = () => {
-  const [username, setUsername] = useState("")
-  const [password, setPassWord] = useState("")
-  const [islogin, setIsLogin] = useState(false)
-  const [err, setErr] = useState(null)
- 
+  
+
   const navigate = useNavigate()
 
-  const handleLogin = async (e) => {
-    e.preventDefault()
-    setIsLogin(true)
+  
+  const handleGoogleLogin = async(response) => {
     try {
-      const res = await newRequest.post("auth/login", {
-        username,
-        password
+      const decodedResponse = jwt_decode(response.credential);
+      const { email, given_name, family_name, picture } = decodedResponse;
+      const res = await newRequest.post("auth/google-login", {
+        username: given_name + ' ' + family_name,
+        email: email,
+        profileImg: picture
 
       })
       localStorage.setItem("currentUser", JSON.stringify(res.data))
-      setIsLogin(false)
       navigate("/")
-    } catch (err) {
-      setErr(err.response.data)
-      setIsLogin(false)
-      console.log(err)
-
-
+    } catch (error) {
+      console.error('Error decoding credentialResponse:', error);
     }
-  }
+
+
+
+  };
+
   return (
     <div className=" flex w-full p-6 min-h-[500px] h-screen justify-center items-center">
-      <form onSubmit={handleLogin} className="flex py-4 px-6 lg:px-8 max-w-[400px] w-full h-fit rounded-lg border-[1px] border-gray-300 shadow-xl flex-col gap-2 justify-center items-center">
+      <form className="flex py-8 px-6 lg:px-8 max-w-[400px] w-full h-fit rounded-lg border-[1px] border-gray-300 shadow-sm hover:shadow-xl flex-col gap-2 justify-center items-center transition-all ease-in-out duration-200">
+        <img src="/assets/CollegeBuzz.png" alt="" />
         <div>
-          <h1 className=" text-xl lg:text-2xl font-bold">Login into your account</h1>
+          <h1 className=" text-lg lg:text-xl font-semibold text-blue-600">Continue with Google</h1>
         </div>
-
-        <div className="mt-8 w-full flex flex-col justify-start items-start gap-1">
-          <h1>Username</h1>
-          <input onChange={(e) => setUsername(e.target.value)} className=" rounded-md h-10 p-2 border-[1px] w-full" type="text" name="username" id="username" placeholder="Enter Username" />
-        </div>
-
-        <div className="w-full flex flex-col justify-start items-start gap-1">
-          <h1>Password</h1>
-          <input onChange={(e) => setPassWord(e.target.value)} className=" rounded-md h-10 p-2 border-[1px] w-full" type="password" name="password" id="password" placeholder="Enter Password" />
-        </div>
-        <div className="mt-2 w-full">
-          <button className=" bg-blue-500 w-full py-2 rounded-md text-white hover:opacity-70 active:opacity-25 transition-all duration-100 ease-in-out" type="submit">
-            {
-              islogin ? "loginin...":"Login"
-            }
-          </button>
-        </div>
-        <div>
-          <p className=" text-red-500 font-semibold">{err && err}</p>
-        </div>
-        <div>
-          <Link to="/register">
-          <p className=" text-sm text-blue-500">Create an Account</p>
-          </Link>
-        </div>
-
-        
-
-
+        <GoogleLogin
+          onSuccess={handleGoogleLogin}
+          onError={() => {
+            alert('Login Failed');
+          }}
+        />
+      
       </form>
     </div>
   )
