f24346165db0cfb70d8cc54668b5fa524a08ffe7
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-08-12 00:31:40 +0530

    myQues loader

--- client/src/pages/myQuestions/MyQuestions.jsx
+++ client/src/pages/myQuestions/MyQuestions.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import newRequest from "../../../utils/newRequest";
 import Feed from "../../components/Feed";
 import { useParams } from "react-router-dom";
+import { ImSpinner9 } from "react-icons/im";
 
 const MyQuestions = () => {
   const user = JSON.parse(localStorage.getItem("currentUser"))
@@ -15,10 +16,10 @@ const MyQuestions = () => {
   });
   return (
     <div className="flex w-full flex-col min-h-full h-auto">
-      <h1 className=" text-3xl font-semibold text-center my-2">Your Questions</h1>
-      <div className="mt-4 relative h-auto w-full flex flex-col gap-4 md:gap-8">
+     
+      <div className="mt-4 relative h-auto w-full flex flex-col items-center gap-4 md:gap-8">
         {
-          isLoading ? (<h2 className=" text-center">Loading Questions...</h2>) :
+          isLoading ? <ImSpinner9 className=" text-2xl animate-spin text-blue-700"/> :
             error ? (<h2 className=" text-center">Something went wrong</h2>) :
               data.length === 0 ? (
                 <div className="mt-12 flex w-full justify-center flex-col items-center">
