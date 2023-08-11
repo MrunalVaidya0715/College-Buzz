cd4177eeb5bc3ebe20aed2c3e72698d0d9b71096
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-08-12 00:28:14 +0530

    Answers loader

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -2,6 +2,7 @@ import Review from "./Review"
 import { useQuery } from "@tanstack/react-query"
 import newRequest from "../../utils/newRequest"
 import { useParams } from "react-router-dom"
+import {ImSpinner9} from 'react-icons/im'
 
 const Reviews = () => {
   const { id } = useParams();
@@ -16,9 +17,9 @@ const Reviews = () => {
   return (
     <div className=" mt-8 w-full flex flex-col gap-2">
       {/* Render Reviews */}
-      <div className=" relative h-auto w-full flex flex-col gap-2 md:gap-4 ">
+      <div className=" relative h-auto w-full flex flex-col items-center gap-2 md:gap-4 ">
         {
-          isLoading ? (<h2 className=" text-center">Loading Answers...</h2>) :
+          isLoading ? <ImSpinner9 className=" text-2xl animate-spin text-blue-700"/> :
             error ? (<h2 className=" text-center">Something went wrong</h2>) :
               data.length === 0 ? (
                 <div className="mt-12 flex w-full justify-center flex-col items-center">
