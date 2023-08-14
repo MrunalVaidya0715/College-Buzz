1ccf446eecbc3b86bcc93531320ad58a57ae1f21
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-08-14 16:53:08 +0530

    BW Filter

--- client/src/pages/contribute/Contribute.jsx
+++ client/src/pages/contribute/Contribute.jsx
@@ -5,48 +5,50 @@ import FilterSort from "../../components/FilterSort"
 import { useEffect, useState } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 import FeedSkeleton from '../../components/FeedSkeleton'
+import useBadwordsFilter from "../../hooks/useBadwordsFilter"
 const Contribute = () => {
-    const { isLoading, error, data, refetch } = useQuery({
-        queryKey: ['question._id'],
-        queryFn: () => newRequest.get(`/questions?category=${cat}&sort=${sort}`).then((res) => {
-          return res.data
-        })
-    
-      })
+  const filter = useBadwordsFilter()
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ['question._id'],
+    queryFn: () => newRequest.get(`/questions?category=${cat}&sort=${sort}`).then((res) => {
+      return res.data
+    })
 
-      const filterPosts = data?.filter((post)=>post.answers.length === 0)
+  })
 
-      const navigate = useNavigate();
-      const location = useLocation();
-      const params = new URLSearchParams(location.search);
-    
-      const [cat, setCat] = useState(params.get("category") || "");
-      const [sort, setSort] = useState(params.get("sort") || "");
-    
-    
-    
-      const handleCat = (e) => {
-        const category = e.target.value;
-        setCat(category);
-    
-        navigate(`?category=${category}&sort=${sort}`);
-      };
-    
-      const handleSort = (e) => {
-        const sorting = e.target.value;
-        setSort(sorting);
-        navigate(`?category=${cat}&sort=${sorting}`);
-      };
-    
-      useEffect(() => {
-        refetch();
-      }, [cat, sort, refetch]);
+  const filterPosts = data?.filter((post) => post.answers.length === 0)
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+
+  const [cat, setCat] = useState(params.get("category") || "");
+  const [sort, setSort] = useState(params.get("sort") || "");
+
+
+
+  const handleCat = (e) => {
+    const category = e.target.value;
+    setCat(category);
+
+    navigate(`?category=${category}&sort=${sort}`);
+  };
+
+  const handleSort = (e) => {
+    const sorting = e.target.value;
+    setSort(sorting);
+    navigate(`?category=${cat}&sort=${sorting}`);
+  };
+
+  useEffect(() => {
+    refetch();
+  }, [cat, sort, refetch]);
   return (
     <>
       <FilterSort handleCat={handleCat} handleSort={handleSort} cat={cat} sort={sort} />
       <div className=" relative h-auto w-full flex flex-col gap-4 md:gap-8">
         {
-          isLoading ? Array(3).fill().map((_, index) => <FeedSkeleton key={index} />):
+          isLoading ? Array(3).fill().map((_, index) => <FeedSkeleton key={index} />) :
             error ? (<h2 className=" text-center">Something went wrong</h2>) :
               filterPosts.length === 0 ? (
                 <div className="mt-12 flex w-full justify-center flex-col items-center">
@@ -55,7 +57,7 @@ const Contribute = () => {
                 </div>
               ) :
                 filterPosts.map((feed) => (
-                  <Feed key={feed._id} refetch={refetch} {...feed} />
+                  <Feed key={feed._id} refetch={refetch} {...feed} title={filter?.clean(feed.title)} desc= {filter?.clean(feed.desc)} />
                 ))
         }
       </div>
