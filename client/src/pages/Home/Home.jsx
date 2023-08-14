53a67ae522e074565972e3cfd759b930b591df08
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-08-14 12:57:04 +0530

    props

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,31 +4,28 @@ import Sidebar from "../../components/Sidebar"
 import { useQuery } from "@tanstack/react-query"
 import newRequest from "../../../utils/newRequest"
 
-const Home = () => {
+const Home = ({ask, handleAsk}) => {
     const { isLoading, error, data } = useQuery({
         queryKey: ['topquestions'],
         queryFn: () => newRequest.get(`questions/top-questions`).then((res) => {
           return res.data
         })
-    
       })
-     
+
     return (
         <div className="pt-16 w-full h-screen bg-slate-100 flex justify-center">
-            <div className="w-full max-w-[1200px] flex lg:gap-4 justify-between">
+            <div className=" w-full max-w-[1200px] flex lg:gap-4 justify-between">
                 <Sidebar />
 
-                {/* <div className=" overflow-y-auto scrollbar scrollbar-w-2 scrollbar-thumb-gray-400 scrollbar-thumb-rounded-lg scrollbar-track-gray-200 h-full w-full flex flex-col  p-2 scroll-smooth">
-                    <Outlet />
-                </div> */}
                 <div className=" overflow-y-auto scrollbar-none h-full w-full flex flex-col  p-2 scroll-smooth">
                     <Outlet />
                 </div>
 
-                <Section isLoading={isLoading} error={error} data={data} />
+                <Section isLoading={isLoading} error={error} data={data}  ask={ask} handleAsk={handleAsk}  />
             </div>
         </div>
     )
 }
+
 
 export default Home
