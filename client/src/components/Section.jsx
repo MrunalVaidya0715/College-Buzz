1fe9dffb687e2a7fcd50b89f9b7c0b291f0446c5
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-08-14 15:23:15 +0530

    some UI Changes

--- client/src/components/Section.jsx
+++ client/src/components/Section.jsx
@@ -42,12 +42,12 @@ const Section = ({ isLoading, error, data }) => {
     return (
         <>
             { isWidget && <div onClick={()=>setIsWidget(false)} className='z-[200] md:hidden fixed top-0 right-0 h-full w-full bg-black/20' />}
-            <div className={` h-fit z-[201] md:z-[0] bg-white md:bg-transparent rounded-l-lg fixed ${isWidget ? "translate-x-0" : "translate-x-[100%]"} md:translate-x-0  md:translate-y-0 right-0 md:static max-w-[75%] sm:max-w-[60%] md:w-[20%] md:min-w-[20%] flex flex-col gap-8 p-2 transition-all ease-in-out duration-500`}>
+            <div className={` h-fit z-[201] md:z-[0] bg-white md:bg-transparent rounded-l-lg fixed ${isWidget ? "translate-x-0" : "translate-x-[100%]"} md:translate-x-0  md:translate-y-0 right-0 md:static max-w-[75%] sm:max-w-[60%] md:w-[20%] md:min-w-[20%] flex flex-col gap-8 py-6 md:py-2 p-2 transition-all ease-in-out duration-500`}>
                 <div onClick={handleWidget} className={`${isWidget ? "opacity-100": "opacity-50"} hover:opacity-100 cursor-pointer md:hidden p-2 rounded-full absolute top-[10%] ${isWidget? "-translate-x-8":"-translate-x-9"} z-[100] bg-blue-700 transition-all ease-in-out duration-500`}>
                     <MdOutlineChevronLeft className={` text-white text-2xl ${isWidget?" rotate-180":""} transition-all ease-in-out duration-500 `} />
                 </div>
                 {/** */}
-                <div className='w-full flex justify-center'>
+                <div className='hidden md:flex w-full justify-center'>
                     {
                         user ? (
                             <button onClick={()=>setAsk(true)}  className=' whitespace-nowrap bg-blue-700 hover:opacity-70 active:opacity-30 hidden md:flex items-center justify-center gap-1 w-full p-2 rounded-md text-white ease-in-out transition-all duration-200'><IoAddOutline size={20} />Ask Question</button>
