18d1d1e6da7401061aa9efcaf24d72d20cf8f959
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-08-14 17:11:52 +0530

    BW Filter

--- client/src/pages/myQuestions/MyQuestions.jsx
+++ client/src/pages/myQuestions/MyQuestions.jsx
@@ -3,10 +3,10 @@ import newRequest from "../../../utils/newRequest";
 import Feed from "../../components/Feed";
 import { useParams } from "react-router-dom";
 import { ImSpinner9 } from "react-icons/im";
-
+import Filter from 'bad-words'
 const MyQuestions = () => {
   const user = JSON.parse(localStorage.getItem("currentUser"))
-  const {userId} = useParams()
+  const { userId } = useParams()
   const { isLoading, error, data } = useQuery({
     queryKey: ["userQuestion"],
     queryFn: () =>
@@ -14,12 +14,26 @@ const MyQuestions = () => {
         return res.data;
       }),
   });
+
+  const { isLoading: isBWLoading, error: BWError, data: badwords } = useQuery({
+    queryKey: ["badwords"],
+    queryFn: () =>
+      newRequest.get(`badwords`).then((res) => {
+        return res.data;
+      }),
+  });
+  const newBadWords = [];
+  if (!(isBWLoading || BWError)) {
+    badwords.map((word) => newBadWords.push(word.word))
+  }
+  const filter = new Filter({ regex: /\*|\.|$/gi })
+  filter.addWords(...newBadWords);
   return (
     <div className="flex w-full flex-col min-h-full h-auto">
-     
+
       <div className="mt-4 relative h-auto w-full flex flex-col gap-4 md:gap-8">
         {
-          isLoading ? (<div className='flex w-full justify-center'><ImSpinner9 className=" text-2xl animate-spin text-blue-700"/></div>) :
+          isLoading ? (<div className='flex w-full justify-center'><ImSpinner9 className=" text-2xl animate-spin text-blue-700" /></div>) :
             error ? (<h2 className=" text-center">Something went wrong</h2>) :
               data.length === 0 ? (
                 <div className="mt-12 flex w-full justify-center flex-col items-center">
@@ -28,7 +42,7 @@ const MyQuestions = () => {
                 </div>
               ) :
                 data.map((feed) => (
-                  <Feed key={feed._id} {...feed} />
+                  <Feed key={feed._id} {...feed} title={filter.clean(feed.title)} desc={filter.clean(feed.desc)} />
                 ))
         }
       </div>
