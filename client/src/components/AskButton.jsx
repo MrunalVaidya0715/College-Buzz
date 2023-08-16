69df2ed7519982d3f7a0800c1ec2c592a2f4735e
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-08-16 20:22:16 +0530

    FIxed AskButn

--- client/src/components/AskButton.jsx
+++ client/src/components/AskButton.jsx
@@ -1,27 +1,18 @@
 import { IoMdAdd } from 'react-icons/io'
 import { useContext } from 'react';
 import { AskButtonContext } from '../context/AskButtonContext'
-import { Link } from 'react-router-dom';
+
 const AskButton = () => {
     const { ask, setAsk } = useContext(AskButtonContext)
-    const user = JSON.parse(localStorage.getItem("currentUser"))
-    return (
-        <>
-            {
-                user ? (
-                    <div onClick={() => setAsk(true)} className={` ${ask ? "hidden" : "block"} cursor-pointer z-[200] fixed md:hidden bottom-4 right-2  p-4 bg-blue-500 rounded-full transition-all ease-in-out duration-1000`}>
-                        <IoMdAdd className=' text-white' size={22} />
-                    </div >
-                ) : (
-                <Link to="/login">
-                    <div className={` ${ask ? "hidden" : "block"} cursor-pointer z-[200] fixed md:hidden bottom-4 right-2  p-4 bg-blue-500 rounded-full transition-all ease-in-out duration-1000`}>
-                        <IoMdAdd className=' text-white' size={22} />
-                    </div >
-                </Link>
-            )
-            }
 
-        </>
+    return (
+
+        <div onClick={() => setAsk(true)} className={` ${ask ? "hidden" : "block"} cursor-pointer z-[200] fixed md:hidden bottom-4 right-2  p-4 bg-blue-500 rounded-full transition-all ease-in-out duration-1000`}>
+            <IoMdAdd className=' text-white' size={22} />
+        </div >
+
+
+
 
     )
 }
