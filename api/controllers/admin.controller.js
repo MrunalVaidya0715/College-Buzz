707433b488f5cac30f0a60c0565144122f7a2393
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-09-10 00:29:35 +0530

    getUsers

--- api/controllers/admin.controller.js
+++ api/controllers/admin.controller.js
@@ -1,5 +1,12 @@
+import User from "../models/user.model.js"
+
 export const getUsers = async(req,res,next)=>{
-    res.status(200).send("Hello")
+    try {
+        const users = await User.find();
+        res.status(200).send(users)
+    } catch (error) {
+        next(error)
+    }
 }
 
 export const getPosts = async(req,res,next)=>{
