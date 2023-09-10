7a8debe6668a5058afb2fdb6a4f895c8c8622e32
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-09-10 16:37:33 +0530

    Some UI Changes

--- client/src/components/admin/UserCard.jsx
+++ client/src/components/admin/UserCard.jsx
@@ -2,8 +2,8 @@ import { formatDistanceToNow } from 'date-fns'
 import { Link } from 'react-router-dom'
 const UserCard = ({_id, username, profileImg, createdAt }) => {
     return (
-        <Link to={`${_id}`}>
-            <div className="rounded-lg bg-white border-[1px] border-gray-300 p-4 flex flex-col items-center justify-center gap-2">
+        <Link className=' rounded-lg bg-white border-[1px] border-gray-300 p-4 flex flex-col items-center justify-center' to={`${_id}`}>
+            <div className=" flex flex-col items-center justify-center gap-2">
                 <img className=" aspect-square rounded-full w-16 h-16 md:w-20 md:h-20" src={profileImg} alt="" />
                 <div className="flex w-full flex-col items-center">
                     <h1 className="text-center font-semibold text-xl">{username}</h1>
