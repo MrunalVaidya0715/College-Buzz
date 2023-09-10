3b96cfeb1fea4029414be91c29dbf481b4c52050
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-09-10 17:56:12 +0530

    Admin Ques apis

--- api/controllers/admin.controller.js
+++ api/controllers/admin.controller.js
@@ -1,14 +1,67 @@
-import User from "../models/user.model.js"
+import User from "../models/user.model.js";
+import Question from "../models/question.model.js";
+import Answer from "../models/answer.model.js";
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).send(users);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getUsers = async(req,res,next)=>{
-    try {
-        const users = await User.find();
-        res.status(200).send(users)
-    } catch (error) {
-        next(error)
+export const getPosts = async (req, res, next) => {
+  res.status(200).send("Hello2");
+};
+
+export const getAdminQuestions = async (req, res, next) => {
+  try {
+    const { category, search, sort } = req.query;
+    let query = {};
+
+    if (category) {
+      query.category = category;
     }
-}
 
-export const getPosts = async(req,res,next)=>{
-    res.status(200).send("Hello2")
-}
+    if (search) {
+      query.title = { $regex: new RegExp(search, "i") };
+    }
+
+    let sortOption = { createdAt: -1 };
+
+    if (sort === "oldest") {
+      sortOption = { createdAt: 1 };
+    }
+
+    const questions = await Question.find(query)
+      .sort(sortOption)
+      .populate("userInfo", "-password");
+
+    res.status(200).json(questions);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteAdminQuestion = async (req, res, next) => {
+  try {
+    const questionId = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(questionId)) {
+      return next(createError(404, "Question Doesn't Exist"));
+    }
+
+    const question = await Question.findById(questionId);
+    if (!question) {
+      return next(createError(404, "Question Doesn't Exist"));
+    }
+
+    //todo: Admin Check for Deleting or not
+
+    await Answer.deleteMany({ questionId });
+
+    await Question.findByIdAndDelete(questionId);
+    res.status(200).send("Question and associated answers have been deleted!");
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/admin.route.js
+++ api/routes/admin.route.js
@@ -1,9 +1,13 @@
 import { Router } from 'express'
-import { getPosts, getUsers } from '../controllers/admin.controller.js'
+import { deleteAdminQuestion, getAdminQuestions, getPosts, getUsers } from '../controllers/admin.controller.js'
+import { verifyToken } from "../middleware/jwt.js";
 const router  = Router()
 
 router.get('/users', getUsers)
 router.get('/posts', getPosts)
+router.get("/", getAdminQuestions)
+router.delete("/:id", verifyToken, deleteAdminQuestion)
+
 
 
 export default router;
