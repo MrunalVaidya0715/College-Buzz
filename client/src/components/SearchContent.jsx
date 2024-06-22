13c90f861429705485343282b99016696f491e2e
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-23 01:46:57 +0530

    feat: Improved SearchResults UI

--- client/src/components/SearchContent.jsx
+++ client/src/components/SearchContent.jsx
@@ -7,31 +7,34 @@ const SearchContent = ({ isLoading, error, data, setSearchQuery }) => {
     return (
 
 
-        <div className='absolute p-2 bg-white w-[80%] max-w-[500px] max-h-[150px] overflow-visible overflow-y-auto scrollbar-none top-36 left-[50%] -translate-x-[50%] -translate-y-[50%] rounded-lg border-[1px] border-gray-300'>
+        <>
+            <div className='fixed top-[4.5rem] left-[50%] -translate-x-[50%] w-[80%] max-w-[500px] flex flex-col gap-2 items-center justify-start overflow-visible   '>
 
-            <div className="flex gap-2 w-full items-center flex-col justify-center">
-                {
-                    isLoading ? <ImSpinner9 className=" text-2xl animate-spin text-blue-700" /> :
-                        error ? <p className="font-semibold">Something went wrong</p> :
-                            data?.length === 0 ? (
-                                <div className="flex gap-2 w-full items-center justify-center">
-                                    <p className="font-semibold">No Questions Found</p>
-                                    <div onClick={()=>setSearchQuery(null)} className="p-2 bg-slate-200 rounded-full">
-                                        <AiOutlineClose />
+                <div className="p-2 flex gap-2 w-full   max-h-[150px] bg-white items-center  justify-start flex-col rounded-lg border-[1px] border-gray-300 overflow-y-auto scrollbar-none">
+                    {
+                        isLoading ? <ImSpinner9 className=" text-2xl animate-spin text-blue-700" /> :
+                            error ? <p className="font-semibold">Something went wrong</p> :
+                                data?.length === 0 ? (
+                                    <div className="py-2 flex gap-2 w-full items-center justify-center">
+                                        <p className="font-semibold">No Questions Found</p>
+
                                     </div>
-                                </div>
-                            ) :
-                                data?.map((res) => (
-                                    <Link onClick={() => setSearchQuery(false)} className="w-full" key={res._id} to={`/posts/${res._id}`}>
-                                        <div className="flex gap-2 items-center hover:bg-slate-100 rounded-md w-full py-1 px-2">
-                                            <img className="w-8 h-8 rounded-full" src={res.userInfo.profileImg} alt="" />
-                                            <p className=" font-semibold">{res.title}</p>
-                                        </div>
-                                    </Link>
-                                ))
-                }
+                                ) :
+                                    data?.map((res) => (
+                                        <Link onClick={() => setSearchQuery(false)} className="w-full" key={res._id} to={`/posts/${res._id}`}>
+                                            <div className="flex gap-2 items-center hover:bg-slate-100 rounded-md w-full py-1 px-2">
+                                                <img className="w-8 h-8 rounded-full" src={res.userInfo.profileImg} alt="" />
+                                                <p className=" font-semibold">{res.title}</p>
+                                            </div>
+                                        </Link>
+                                    ))
+                    }
+                </div>
+                <div onClick={() => setSearchQuery(null)} className=" cursor-pointer p-2 w-fit bg-slate-200 border-[1px] border-gray-400 rounded-full">
+                    <AiOutlineClose />
+                </div>
             </div>
-        </div>
+        </>
 
     )
 }
